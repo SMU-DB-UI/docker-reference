4e0bf483085357f6f49595f1423187984df250db
Mark Fontenot <mfonten@lyle.smu.edu>
2019-10-21 11:48:27 -0500

    added parts to communicate between the two cntrs

--- src/server.js
+++ src/server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'db',
+  port: '3306',
+  user: 'user',
+  password: 'password', 
+  database: 'db'
+});
 
 const config = {
     name: 'sample-express-app',
     port: 3000,
     host: '0.0.0.0',
 };
+
+
 
 const app = express();
 const logger = log({ console: true, file: false, label: config.name });
@@ -16,8 +27,28 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
+connection.connect (function(err) {
+  if (err)
+    logger.error("Cannot connect to DB!");
+  logger.info("Connected to the DB!");
+});
+
 app.get('/', (req, res) => {
+  // connection.connect (function(err) {
+  //   if (err) throw err;
+  //   console.log("Connected!");
+  // });
+    connection.query('SELECT * from data', function (err, rows, fields) {
+      
+      if(err) {
+        logger.error("Error while executing Query");
+      };
+      logger.info(rows.length);
+      logger.info(rows[0].name + ' ' + rows[0].id);
+      logger.info(fields.length);
+    })
     res.status(200).send('Hellooooo, still here!?!?');
+  // connection.end();
 });
 
 app.listen(config.port, config.host, (e)=> {
