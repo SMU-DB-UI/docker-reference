0e12489add90c03bfadaeec8bce01bff2d543483
Mark <markelliottfontenot@gmail.com>
2019-10-24 06:53:36 -0500

    updated server.js route handlers

--- src/server.js
+++ src/server.js
@@ -1,59 +1,96 @@
+/**A simple node/express server that include communication with a 
+ * mysql db instance. 
+*/
+
+//create main objects
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 const mysql = require('mysql');
 
+
+//create the mysql connection object.  
 var connection = mysql.createConnection({
+  //db is the host and that name is assigned based on the 
+  //container name given in the docker-compose file
   host: 'db',
   port: '3306',
   user: 'user',
-  password: 'password', 
+  password: 'password',
   database: 'db'
 });
 
+//set up some configs for express. 
 const config = {
-    name: 'sample-express-app',
-    port: 3000,
-    host: '0.0.0.0',
+  name: 'sample-express-app',
+  port: 3000,
+  host: '0.0.0.0',
 };
 
-
-
+//create the express.js object
 const app = express();
+
+//create a logger object.  Using logger is preferable to simply writing to the console. 
 const logger = log({ console: true, file: false, label: config.name });
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-connection.connect (function(err) {
+//Attempting to connect to the database.
+connection.connect(function (err) {
   if (err)
     logger.error("Cannot connect to DB!");
   logger.info("Connected to the DB!");
 });
 
+/**     REQUEST HANDLERS        */
+
+//GET /
 app.get('/', (req, res) => {
-  // connection.connect (function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  // });
-    connection.query('SELECT * from data', function (err, rows, fields) {
-      
-      if(err) {
-        logger.error("Error while executing Query");
-      };
-      logger.info(rows.length);
-      logger.info(rows[0].name + ' ' + rows[0].id);
-      logger.info(fields.length);
-    })
-    res.status(200).send('Hellooooo, still here!?!?');
-  // connection.end();
+  res.status(200).send('Go to localhost:3000/setupdb first. Then Go to localhost:3000/checkdb');
 });
 
-app.listen(config.port, config.host, (e)=> {
-    if(e) {
-        throw new Error('Internal Server Error');
-    }
-    logger.info(`${config.name} running on ${config.host}:${config.port}`);
+
+//GET /setupdb
+app.get('/setupdb', (req, res) => {
+  connection.query('drop table if exists data2', function (err, rows, fields) {
+    if (err)
+      logger.error("Can't drop table");
+  });
+  connection.query('create table data2(id int, name varchar(50))', function (err, rows, fields) {
+    if (err)
+      logger.error("Problem creating the table data2");
+  });
+  connection.query('insert into data2 values(1, \'mark\')', function(err, rows, fields) {
+      if(err)
+        logger.error('adding row to table failed');
+  });
+  res.status(200).send('created the table');
+});
+
+//GET /checkdb
+app.get('/checkdb', (req, res) => {
+  //execute a query to select * from table named data. 
+  connection.query('SELECT * from data2', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+    };
+    logger.info(rows[0].name + ' ' + rows[0].id);
+ 
+    //writing to the response object
+    res.type('text/html');
+    res.status(200);
+    res.send('<h1>' + rows[0].id + ' ' + rows[0].name + '</h1>');
+  })
+});
+
+//connecting the express object to listen on a particular port as defined in the config object. 
+app.listen(config.port, config.host, (e) => {
+  if (e) {
+    throw new Error('Internal Server Error');
+  }
+  logger.info(`${config.name} running on ${config.host}:${config.port}`);
 });
